dde4edcf588f6d60a0b51e9c9666da86b1b7433d
Deva0813 <devanand151101@gmail.com>
2026-08-04 21:05:07 +0530

    baseUrl fix

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   ...(process.env.NODE_ENV == "production" ? {
     output: 'export',
-    basePath: 'deva0813.github.io/portfolio',
+    basePath: '/portfolio',
     images: {
       unoptimized: true,
     },
